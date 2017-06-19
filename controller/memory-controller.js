e8d8c63ac3464809b50c373f67f50a26d7fb82b2
Abigail White <abigailwhite213@gmail.com>
2017-06-19 15:01:49 -0700

    working on memory controller and routes

---
+++ controller/memory-controller.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const createError = require('http-errors');
+const Promise = require('bluebird');
+const Memory = require('../model/memory');
+
+module.exports = exports = {};
+
+exports.createMemory = function(req)  {
+  if(!req.body.name) return Promise.reject(createError(400, 'Invalid name property'));
+  if(!req.body.desc) return Promise.reject(createError(400, 'Invalid desc property'));
+
+  req.body.userId = req.user._id;
+
+  return new Memory(req.body).save()
+  .then(memory => memory)
+  .catch(err => Promise.reject(createError(400, err.message)));
+};
+
+exports.getMemory = function(req) {
+  if(!req.params.id) return Promise.reject(createError(400, 'ID required'));
+
+  return Memory.findById(req.params.id)
+  .catch(err => console.error(err));
+};
+
+exports.updateMemory = function(req) {
+  if(!req.params.id) return Promise.reject(createError(400, 'ID required'));
+
+  return Memory.findOneAndUpdate(req.params.id, req.body, {new: true});
+};
+
+exports.deleteMemory = function(req, res, id){
+  if(!id) return Promise.reject(createError(400, 'ID required'));
+
+  return Memory.findByIdAndRemove(id);
+};
+
+exports.getMap = function(req){
+
+};
--- routes/memory-routes.js
+++ routes/memory-routes.js
@@ -5,36 +5,52 @@ const bearerAuth = require('../lib/bearer-auth-middleware.js');
 const memoryCtrl = require('../controller/memory-controller.js');
 
 module.exports = function(router){
-  router.post('/api/memory', bearerAuth, (req, res) => {
-    debug('#POST /api/memory');
+  router.post('/memory', bearerAuth, (req, res) => {
+    debug('#POST /memory');
 
     memoryCtrl.createMemory(req)
     .then(memory => res.json(memory))
     .catch(err => res.status(err.status).send(err.name));
 
   });
-  // router.post('/api/memory', bearerAuth, (req, res) => {
-  //   debug('#POST /api/memory');
-  //
-  //
-  // });
-  // router.post('/api/memory', bearerAuth, (req, res) => {
-  //   debug('#POST /api/memory');
-  //
-  //
-  // });
-  // router.post('/api/memory', bearerAuth, (req, res) => {
-  //   debug('#POST /api/memory');
-  //
-  //
-  // });
-  // router.post('/api/memory', bearerAuth, (req, res) => {
-  //   debug('#POST /api/memory');
-  //
-  //
-  // });
 
+  router.get('/memory/user_id', bearerAuth, (req, res) => {
+    debug('#POST /memory');
 
+    memoryCtrl.getMemory(req)
+    .then(gallery => res.json(gallery))
+    .catch(err => {
+      res.status(err.status).send(err.message);
+    });
+  });
 
+  router.put('/memory/:id', bearerAuth, (req, res) => {
+    debug('#POST /memory');
+
+    memoryCtrl.updateMemory(req)
+    .then(pic => res.json(pic))
+    .catch(err => res.status(err.status).send(err.message));
+
+  });
+
+  router.delete('/memory', bearerAuth, (req, res) => {
+    debug('#POST /memory');
+
+    memoryCtrl.deleteMemory(req, res, req.params.id)
+    .then(() => res.status(204).send())
+    .catch(err => res.send(err));
+
+  });
+
+  router.get('/map', bearerAuth, (req, res) => {
+    debug('#POST /memory');
+
+    memoryCtrl.getMap(req)
+    .then(gallery => res.json(gallery))
+    .catch(err => {
+      res.status(err.status).send(err.message);
+    });
+  });
+  
   return router;
 };
