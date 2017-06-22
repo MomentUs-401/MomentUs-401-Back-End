ece5e821ae4542ad8eaf70ea16ab3eca01238de6
Abigail White <abigailwhite213@gmail.com>
2017-06-22 08:16:31 -0700

    default image create

--- model/memory.js
+++ model/memory.js
@@ -7,14 +7,14 @@ const memorySchema = Schema({
   title: {type: String, required: true, maxlength: 100},
   date: {type: Date, required: true},
   location: {
-    lat: {type: Number, required: true},
-    lng: {type: Number, required: true},
+    lat: {type: Number},
+    lng: {type: Number},
   },
   description: {type: String, required: true, maxlength: 1000},
   songTitle: {type: String, maxlength: 100},
   photo: {
-    imageURI: {type: String, required: true},
-    ObjectId: {type: String, required: true},
+    imageURI: {type: String, default:'https://s3-us-west-2.amazonaws.com/lab-18/00d7421a7240fec66f1b33598c358c12.png'},
+    ObjectId: {type: String, default:'6b2dc6dc5f31a5db65211c95bfd2b761.png'},
   },
   friends: {type: String, maxlength: 200},
   dateCreated: {type: Date, default: Date.now, required: true},
