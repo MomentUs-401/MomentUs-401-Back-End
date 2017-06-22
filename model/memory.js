5928739777e4957bdc1e25373f1cf16b4a05de33
Abigail White <abigailwhite213@gmail.com>
2017-06-21 17:04:21 -0700

    update memory model

--- model/memory.js
+++ model/memory.js
@@ -6,7 +6,10 @@ const Schema = mongoose.Schema;
 const memorySchema = Schema({
   title: {type: String, required: true, maxlength: 100},
   date: {type: Date, required: true},
-  location: {type: String, required: true},
+  location: {
+    lat: {type: Number},
+    lng: {type: Number}
+  },
   description: {type: String, required: true, maxlength: 1000},
   songTitle: {type: String, maxlength: 100},
   photo: {
